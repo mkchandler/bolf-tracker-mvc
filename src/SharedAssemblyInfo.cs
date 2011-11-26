51ce5c2ff43548d506adc167c9719f16fc177429
Matt Chandler <mkchandler@gmail.com>
2011-11-26 11:29:21 -0600

    Upped version number

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bolf Tracker")]
 [assembly: AssemblyCopyright("Copyright © Matt Chandler and contributors")]
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0")]
