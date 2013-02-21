c3e70b01ce64dc86afeebbe9e161e209d0cec6e8
Matt Chandler <mkchandler@gmail.com>
2013-02-20 18:20:08 -0600

    Turned off com visibility

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,6 +4,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Bolf Tracker")]
 [assembly: AssemblyProduct("Bolf Tracker")]
 [assembly: AssemblyCopyright("Copyright © 2013 Matt Chandler and contributors")]
+
+[assembly: ComVisible(false)]
+
 [assembly: AssemblyVersion("0.9.2")]
 [assembly: AssemblyFileVersion("0.9.2")]
 [assembly: AssemblyInformationalVersion("0.9.2-beta")]
