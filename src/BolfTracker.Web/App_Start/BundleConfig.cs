a1669cf9bd436a303e81c0ece799261056d13489
unknown <radiofreak311@gmail.com>
2013-09-19 14:40:40 -0500

    career stat chart

--- src/BolfTracker.Web/App_Start/BundleConfig.cs
+++ src/BolfTracker.Web/App_Start/BundleConfig.cs
@@ -10,11 +10,14 @@ namespace BolfTracker.Web
             bundles.Add(new ScriptBundle("~/bundles/js")
                    .Include("~/js/jquery-1.9.1.js")
                    .Include("~/js/bootstrap.js")
-                   .Include("~/js/jquery.tablesorter.js"));
+                   .Include("~/js/jquery.tablesorter.js")
+                   .Include("~/Scripts/d3.v3.min.js")
+                   .Include("~/Scripts/chart.js"));
 
             bundles.Add(new StyleBundle("~/bundles/css")
                    .Include("~/css/bootstrap.css")
-                   .Include("~/css/bolftracker.css"));
+                   .Include("~/css/bolftracker.css")
+                   .Include("~/css/chart.css"));
         }
     }
 }
