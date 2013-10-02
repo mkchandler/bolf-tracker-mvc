d0aa0b2779eeb67b27423832fc6ff52af75e064a
Matt Chandler <mkchandler@gmail.com>
2013-10-02 18:07:19 -0500

    Upped the patch version

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.11.0")]
-[assembly: AssemblyFileVersion("0.11.0")]
-[assembly: AssemblyInformationalVersion("0.11.0")]
+[assembly: AssemblyVersion("0.11.1")]
+[assembly: AssemblyFileVersion("0.11.1")]
+[assembly: AssemblyInformationalVersion("0.11.1-beta")]
