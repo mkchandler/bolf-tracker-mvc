bc975f6fef8c213fd1f16d28164f5f6265825979
Matt Chandler <mkchandler@gmail.com>
2014-06-10 21:48:58 -0500

    Upgrade MVC and removed unused packages

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.12.0")]
 [assembly: AssemblyFileVersion("0.12.0")]
-[assembly: AssemblyInformationalVersion("0.12.0-beta")]
+[assembly: AssemblyInformationalVersion("0.12.0")]
