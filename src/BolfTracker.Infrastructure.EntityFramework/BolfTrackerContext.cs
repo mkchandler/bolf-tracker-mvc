7fe3f2caa59bb2e088562340d42d78a24ff132cf
Matt Chandler <mkchandler@gmail.com>
2019-03-03 16:42:41 -0600

    Cleaned up the connection string builder code

--- src/BolfTracker.Infrastructure.EntityFramework/BolfTrackerContext.cs
+++ src/BolfTracker.Infrastructure.EntityFramework/BolfTrackerContext.cs
@@ -1,5 +1,4 @@
 ﻿using System.Data.Entity;
-
 using BolfTracker.Models;
 
 namespace BolfTracker.Infrastructure.EntityFramework
--- src/BolfTracker.Infrastructure.EntityFramework/ConnectionString.cs
+++ src/BolfTracker.Infrastructure.EntityFramework/ConnectionString.cs
@@ -6,18 +6,12 @@ namespace BolfTracker.Infrastructure.EntityFramework
     {
         public static string Build()
         {
-            var appConfig = ConfigurationManager.AppSettings;
+            string hostname = ConfigurationManager.AppSettings["RDS_HOSTNAME"];
+            string database = ConfigurationManager.AppSettings["RDS_DB_NAME"];
+            string username = ConfigurationManager.AppSettings["RDS_USERNAME"];
+            string password = ConfigurationManager.AppSettings["RDS_PASSWORD"];
 
-            string dbname = appConfig["RDS_DB_NAME"];
-
-            if (string.IsNullOrEmpty(dbname)) return null;
-
-            string username = appConfig["RDS_USERNAME"];
-            string password = appConfig["RDS_PASSWORD"];
-            string hostname = appConfig["RDS_HOSTNAME"];
-            string port = appConfig["RDS_PORT"];
-
-            return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
+            return $"Data Source={hostname};Initial Catalog={database};User ID={username};Password={password};";
         }
     }
 }
