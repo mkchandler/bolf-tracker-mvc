370c50b34769bb2dd8c3a6004da7ab09eaca0289
Matt Chandler <mkchandler@gmail.com>
2019-03-03 20:33:08 -0600

    Use local SQL server connection string in Debug builds

--- src/BolfTracker.Infrastructure.EntityFramework/ConnectionString.cs
+++ src/BolfTracker.Infrastructure.EntityFramework/ConnectionString.cs
@@ -6,12 +6,16 @@ namespace BolfTracker.Infrastructure.EntityFramework
     {
         public static string Build()
         {
+#if DEBUG
+            return ConfigurationManager.ConnectionStrings["BolfTracker"].ConnectionString;
+#else
             string hostname = ConfigurationManager.AppSettings["RDS_HOSTNAME"];
             string database = ConfigurationManager.AppSettings["RDS_DB_NAME"];
             string username = ConfigurationManager.AppSettings["RDS_USERNAME"];
             string password = ConfigurationManager.AppSettings["RDS_PASSWORD"];
 
             return $"Data Source={hostname};Initial Catalog={database};User ID={username};Password={password};";
+#endif
         }
     }
 }
